6b93ed6e8aa58932fcab9bf3ad269cbb2619b1de
Andrew Efurhievwe <andyriles22@gmail.com>
2022-12-28 10:23:01 +0100

    first commit

--- dist/config/config.js
+++ dist/config/config.js
@@ -27,10 +27,10 @@ const MONGO = {
     url: MONGO_URL,
 };
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
-const SERVER_PORT = process.env.SERVER_PORT || 8000;
+const PORT = process.env.PORT || 8000;
 const SERVER = {
     hostname: SERVER_HOSTNAME,
-    port: SERVER_PORT,
+    port: PORT,
 };
 const config = {
     server: SERVER,
